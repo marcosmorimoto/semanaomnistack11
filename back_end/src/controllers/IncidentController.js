e37b9b025bb8fb2a23f7a89c842954b20ae00961
MORIMOTO.EXT Marcos (EXTERNAL) <marcos.morimoto.ext@sodexo.com>
2020-04-07 21:18:46 -0300

    Fix back end

--- back_end/src/controllers/IncidentController.js
+++ back_end/src/controllers/IncidentController.js
@@ -6,10 +6,8 @@ module.exports = {
 
         const [ count ] = await connection('incidents').count();
 
-        console.log(count);
-
         const incidents = await connection('incidents')
-            .join('ongs','ong_id', '=', 'incidents.ong_id')
+            .join('ongs','ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
             .select([
@@ -47,7 +45,7 @@ module.exports = {
             .select('ong_id')
             .first();
 
-        if(incident.ong_id != ong_id){
+        if(incident.ong_id !== ong_id){
             return response.status(401).json({ error: 'Operation not permitted.' });
         }
 
